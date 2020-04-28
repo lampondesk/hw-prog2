1b0ddbaa32cc976b4f82545cc9b10e50d63880f4
Samuel Ch <me@chsamuel.net>
2020-04-28 12:00:12 +0200

    Created cStructures.hpp

---
+++ source/cStructures.hpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+class cContact {
+    int            sqlId;
+    std::string    firstName;
+    std::string    lastName;
+    std::string    tel;
+    std::string    email;
+    std::string    comments;
+    std::string    addrStreet;
+    std::string    addrNum;
+    std::string    town;
+    std::string    state;
+    std::string    country;
+    int            postal;
+
+    int sApplyToDB(int s) { }
+    int cValidateData(int type) { }
+
+ public:
+    cContact(std::string stn = "", std::string lan = "", std::string tel = "", std::string em = "", std::string comm = "", std::string as = "", std::string an = "",
+    std::string to = "", std::string sta = "", std::string co = "", int po = 0) : firstName(stn), lastName(lan), tel(tel), email(em), comments(comm), addrStreet(as),
+    addrNum(an), town(to), state(sta), country(co), postal(po) { }
+
+    int setData(std::string stn, std::string l, std::string te, std::string em, std::string comm, std::string as, std::string an,
+    std::string to, std::string sta, std::string co, int po) { }
+    void displayData() { }
+    bool matchData() { }
+    char* prepareVCard() { }
+    ~cContact();
+};
+
+class contactStore {
+    cContact*    store;
+    size_t      quantity;
+
+ public:
+    contactStore();
+    int addContact();
+    int delContact();
+    int setContact();
+    void displayAll();
+    void search();
+    int exportVCard();
+    ~contactStore();
+};
+
