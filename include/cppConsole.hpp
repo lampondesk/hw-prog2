58a0665de0f7dae254178686c014940e5ab91ae1
Samuel Ch <me@chsamuel.net>
2020-05-01 11:44:59 +0200

    Declared loop functionality

--- include/cppConsole.hpp
+++ include/cppConsole.hpp
@@ -2,8 +2,11 @@
 
 class TerminalHandler {
 	std::string prompt;
+	bool loop;
 public:
 	TerminalHandler(std::string p = "") : prompt(p) {}
 	int getCommand();
+	bool getLoop() { return loop; }
+	void setLoop(bool b) { loop = b; }
 	std::string lastCommand();
 };
