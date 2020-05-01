6556a3fc3bbfefb49f78d380386805db6db8d637
Samuel Ch <me@chsamuel.net>
2020-05-01 11:59:08 +0200

    add default value to bool loop

--- include/cppConsole.hpp
+++ include/cppConsole.hpp
@@ -4,7 +4,7 @@ class TerminalHandler {
 	std::string prompt;
 	bool loop;
 public:
-	TerminalHandler(std::string p = "") : prompt(p) {}
+	TerminalHandler(std::string p = "", bool b = true) : prompt(p), loop(b) {}
 	int getCommand();
 	bool getLoop() { return loop; }
 	void setLoop(bool b) { loop = b; }
