75e788efa25d3931b4fbf9785cd248ae73c3c2ce
Samuel Ch <me@chsamuel.net>
2020-05-01 12:06:59 +0200

    Added instance of TerminalHandler and main loop

--- source/main.cpp
+++ source/main.cpp
@@ -3,9 +3,24 @@
 #include <cppStructures.hpp>
 #include <cppStatus.hpp>
 #include <cppExporter.hpp>
+#include <cppConsole.hpp>
 
 int main() {
+
+	/*					Initialisation: objects					*/
+	TerminalHandler		console;
+
+
+	/*					Initialisation: main loop				*/
     std::cout << "cppKontakte v.0.0.1" << std::endl;
     setlocale(LC_ALL, "");
+
+	while(console.getLoop()) {
+		console.getCommand();
+		if (console.lastCommand() == "-exit") {
+			console.setLoop(false);
+		}
+	}
+
     return 0;
 }
