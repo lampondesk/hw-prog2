fd53345d128e54d722cb35f15418ffb73080624e
Samuel Ch <me@chsamuel.net>
2020-05-11 13:09:39 +0200

    Added file initialisation and read

--- source/main.cpp
+++ source/main.cpp
@@ -4,18 +4,28 @@
 #include <cppStatus.hpp>
 #include <cppExporter.hpp>
 #include <cppConsole.hpp>
+#include <cppImport.hpp>
 #include <memtrace.h>
 
-int main() {
+int main(int argc, char** argv) {
 
 	/*					Initialisation: objects					*/
 	TerminalHandler		console;
-	int					DEBUG = 0;
+	ImportHandler		ifile;
+	contactStore		memStorage;
+	bool				DEBUG = false;
 
 
 	/*					Initialisation: main loop				*/
     std::cout << "cppKontakte v.0.0.1" << std::endl;
     setlocale(LC_ALL, "");
+
+	if (argc >= 3 && strcmp(argv[1], "-f") == 0) {
+		ifile.initFile(std::string(argv[2]));
+	} else {
+		ifile.initFile("cppkontakte.dat.csv");
+	}
+	ifile.readContacts(memStorage);
 
 	while(console.getLoop()) {
 		console.getCommand();
