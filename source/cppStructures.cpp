a5aa172156a5bb89bf3c7dc5b32ac0720308f7ea
Samuel Ch <me@chsamuel.net>
2020-05-12 13:00:57 +0200

    Fixed contactStore mem leak

--- source/cppStructures.cpp
+++ source/cppStructures.cpp
@@ -15,81 +15,57 @@ cppContact::cppContact(const cppContact& rhs) {
 	this->postal = rhs.postal;
 	this->id = rhs.id;
 }
-const cppContact &cppContact::operator=(const cppContact& rhs) {
-	return rhs;
-}
+
 void cppContact::setData(const char* data, int field) {
 	std::string temp(data);
 	if (field == 0) {
 		this->id = 12; //Replace with atoi(data)
 	} else if (field == 1) {
-		this->firstName = data;
+		this->firstName = temp;
 	} else if (field == 2) {
-		this->lastName = data;
+		this->lastName = temp;
 	} else if (field == 3) {
-		this->tel = data;
+		this->tel = temp;
 	} else if (field == 4) {
-		this->email = data;
+		this->email = temp;
 	} else if (field == 5) {
-		this->comments = data;
+		this->comments = temp;
 	} else if (field == 6) {
-		this->addrStreet = data;
+		this->addrStreet = temp;
 	} else if (field == 7) {
-		this->addrNum = data;
+		this->addrNum = temp;
 	} else if (field == 8) {
-		this->town = data;
+		this->town = temp;
 	} else if (field == 9) {
-		this->state = data;	
+		this->state = temp;	
 	} else if (field == 10) {
-		this->country = data;
+		this->country = temp;
 	} else if (field == 11) {
 		this->postal = 9; //Replace with atoi(data)
 	} else {
 		throw CPPKONTAKTE_ILLEGAL_DATA_REQUEST;
 	}
 }
-void cppContact::getData(int& target, int field) {
-	if (field == 0) {
-		target = this->id;
-	} else if (field == 11) {
-		target = this->postal;
-	} else {
-		throw CPPKONTAKTE_ILLEGAL_DATA_REQUEST;
-	}
-}
-void cppContact::getData(std::string& target, int field) {
-	if (field == 1) {
-		target = this->firstName;
-	} else if (field == 2) {
-		target = this->lastName;
-	} else if (field == 3) {
-		target = this->tel;
-	} else if (field == 4) {
-		target = this->email;
-	} else if (field == 5) {
-		target = this->comments;
-	} else if (field == 6) {
-		target = this->addrStreet;
-	} else if (field == 7) {
-		target = this->addrNum;
-	} else if (field == 8) {
-		target = this->town;
-	} else if (field == 9) {
-		target = this->state;	
-	} else if (field == 10) {
-		target = this->country;
-	} else {
-		throw CPPKONTAKTE_ILLEGAL_DATA_REQUEST;
-	}
-}
 
+/*
+*	CONTACTSTORE
+*/
 
-void contactStore::addContact(cppContact&) {
-	cppContact* temp = new cppContact[this->quantity++];
-	for (size_t i = 0; i < (quantity - 1); i++) {
+void contactStore::addContact(cppContact& contact) {
+	cppContact* temp = new cppContact[this->quantity + 1];
+	size_t i;
+	for (i = 0; i < quantity; i++) {
 		temp[i] = cppContact(this->store[i]);
 	}
-	if ((quantity - 1) != 0)
-		delete this->store;
+	temp[i] = contact;
+	if (quantity != 0)
+		delete[] this->store;
+	quantity++;
 	this->store = temp;
+}
+cppContact& contactStore::operator[](size_t index) {
+	return this->store[index];
+}
+const cppContact& contactStore::operator[](size_t index) const {
+	return (const cppContact&)this->store[index];
 }
