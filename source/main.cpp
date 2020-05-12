f517150e77284b379510eb926b9d56f14cb2c325
Samuel Ch <me@chsamuel.net>
2020-05-12 13:20:09 +0200

    do not auto open files, added open command

--- source/main.cpp
+++ source/main.cpp
@@ -21,18 +21,6 @@ int main(int argc, char** argv) {
     std::cout << "cppKontakte v.0.2.25" << std::endl;
     setlocale(LC_ALL, "");
 
-	if (argc >= 3 && strcmp(argv[1], "-f") == 0) {
-		ifile.initFile(std::string(argv[2]));
-	} else {
-		ifile.initFile("cppkontakte.dat.csv");
-	}
-	try {
-		ifile.readContacts(memStorage);
-	} catch (int) {
-		std::cout << "The file seems to be corrupt." << std::endl;
-		return CPPKONTAKTE_FILE_CORRUPT;
-	}
-
 	while(console.getLoop()) {
 		console.getCommand();
 		if (console.lastCommand() == "-exit") {
@@ -40,10 +28,26 @@ int main(int argc, char** argv) {
 		} else if (console.lastCommand() == "-debug") {
 			DEBUG = !DEBUG;
 			std::cout << "[Debug] Toggled debug mode." << std::endl;
+		} else if (console.lastCommand() == "open") {
+			if (argc >= 3 && strcmp(argv[1], "-f") == 0) {
+				ifile.initFile(std::string(argv[2]));
+			} else {
+				ifile.initFile("cppkontakte.dat.csv");
+			}
+			try {
+				ifile.readContacts(memStorage);
+			} catch (int) {
+				std::cout << "The file seems to be corrupt. Aborting." << std::endl;
+				return CPPKONTAKTE_FILE_CORRUPT;
+			}
 		} else if (console.lastCommand() == "export csv") {
-			Exporter ocsv;
-			ocsv.initFile("oucsv.csv");
-			ocsv.outCsv(true, memStorage);
+			if (ifile.isOpen()) {
+				Exporter ocsv;
+				ocsv.initFile("oucsv.csv");
+				ocsv.outCsv(true, memStorage);
+			} else {
+				std::cout << "No file has been opened yet." << std::endl;
+			}
 		}
 	}
 
