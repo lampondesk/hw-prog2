94a9d82b77c4c4044b7f508c05a2369c2c174947
Samuel Ch <me@chsamuel.net>
2020-05-16 22:09:28 +0200

    Added validator and console functions

--- include/cppConsole.hpp
+++ include/cppConsole.hpp
@@ -1,11 +1,20 @@
 #include <iostream>
+#include <cppStructures.hpp>
 
 class TerminalHandler {
 	std::string prompt;
 	bool loop;
+	void validateEmail(std::string) { };
+	void validatePhone(std::string) { };
+	void validateNoNum(std::string) { };
+	void validateOnlyNum(std::string) { };
 public:
-	TerminalHandler(std::string p = "", bool b = true) : prompt(p), loop(b) {}
+	TerminalHandler(bool b = true) : loop(b) {}
 	void getCommand();
+	void consoleAdd(contactStore& cs);
+	void consoleEdit(contactStore& cs) { };
+	void consoleDelete(contactStore& cs) { };
+	void consoleSearch(contactStore& cs);
 	bool getLoop() { return loop; }
 	void setLoop(bool b) { loop = b; }
 	std::string lastCommand() { return prompt; }
