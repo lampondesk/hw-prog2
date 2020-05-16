d34188ab02e00ff4cc2510e500e113a33696bffa
Samuel Ch <me@chsamuel.net>
2020-05-16 22:11:17 +0200

    Use new functions and bumped version

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <locale>
-#include <string.h>
 #include <cppStructures.hpp>
 #include <cppStatus.hpp>
 #include <cppExporter.hpp>
@@ -13,8 +12,20 @@ int main(int argc, char** argv) {
 	ImportHandler		ifile;
 	contactStore		memStorage;
 	bool				DEBUG = false;
+	std::string			currentfile;
 
-    std::cout << "cppKontakte v.0.3.3" << std::endl;
+    std::cout << "cppKontakte v.0.7.1" << std::endl;
+	std::cout << "General commands are: " << std::endl;
+	std::cout << "add\t\t | Add a new person to the contact list." << std::endl;
+	std::cout << "edit\t\t | Edit an already existing person." << std::endl;
+	std::cout << "delete\t\t | Delete an already existing person." << std::endl;
+	std::cout << "search\t\t | Search for exact or similar data." << std::endl;
+	std::cout << "File management commands are: " << std::endl;
+	std::cout << "open\t\t | Open the default file or the one supplied via the '-f' option." << std::endl;
+	std::cout << "export csv\t | Export the contact list into a stadard csv file." << std::endl;
+	std::cout << "export vcard\t | Export the contact list into a stadard VCard file." << std::endl;
+	std::cout << "-exit\t\t | Close the application." << std::endl;
+	std::cout << "Note: the file is saved automatically after each change." << std::endl << std::endl;
     setlocale(LC_ALL, "");
 
 	while(console.getLoop()) {
@@ -25,33 +36,111 @@ int main(int argc, char** argv) {
 			DEBUG = !DEBUG;
 			std::cout << "[Debug] Toggled debug mode." << std::endl;
 		} else if (console.lastCommand() == "open") {
-			if (argc >= 3 && strcmp(argv[1], "-f") == 0) {
-				ifile.initFile(std::string(argv[2]));
-			} else {
-				ifile.initFile("cppkontakte.dat.csv");
-			}
 			try {
+				if (argc >= 3 && std::string(argv[1]) == "-f") {
+					currentfile = std::string(argv[2]);
+					ifile.initFile(currentfile);
+				} else {
+					currentfile = "cppkontakte.dat.csv";
+					ifile.initFile(currentfile);
+				}
 				ifile.readContacts(memStorage);
-			} catch (int) {
-				std::cout << "The file seems to be corrupt. Aborting." << std::endl;
-				return CPPKONTAKTE_FILE_CORRUPT;
+			} catch (int e) {
+				if (e == CPPKONTAKTE_FILE_IO_ERROR) {
+					std::cout << "Error: the requested or the default file cannot be opened." << std::endl;
+				} else if (e == CPPKONTAKTE_FILE_CORRUPT) {
+					std::cout << "Fatal error: the requested or the default file is corrupt.\nThe app will exit now." << std::endl;
+					return CPPKONTAKTE_FILE_CORRUPT;
+				} else if (e == CPPKONTAKTE_MEMORY_ALLOC_ERROR) {
+					std::cout << "Fatal error: memory error: this either means that there is a bug\nin the software, or you are out of memory." << std::endl;
+				}
 			}
 		} else if (console.lastCommand() == "export csv") {
 			if (ifile.isOpen()) {
-				Exporter ocsv;
-				ocsv.initFile("oucsv.csv");
-				ocsv.outCsv(true, memStorage);
+				try {
+					Exporter ocsv;
+					ocsv.initFile("oucsv.csv");
+					ocsv.outCsv(true, memStorage);
+					ocsv.~Exporter();
+				} catch (int e) {
+					if (e == CPPKONTAKTE_FILE_IO_ERROR) {
+						std::cout << "Error: export failed: export target file cannot be opened.";
+					}
+				}
 			} else {
 				std::cout << "No file has been opened yet." << std::endl;
 			}
 		} else if (console.lastCommand() == "export vcard") {
 			if (ifile.isOpen()) {
-				Exporter ovcf;
-				ovcf.initFile("ouvcard.vcf");
-				ovcf.outVcard(memStorage);
+				try {
+					Exporter ovcf;
+					ovcf.initFile("ouvcard.vcf");
+					ovcf.outVcard(memStorage);
+					ovcf.~Exporter();
+				} catch (int e) {
+					if (e == CPPKONTAKTE_FILE_IO_ERROR) {
+						std::cout << "Error: export failed: export target file cannot be opened.";
+					}
+				}
 			} else {
 				std::cout << "No file has been opened yet." << std::endl;
 			}
+		} else if (console.lastCommand() == "new") {
+			if(ifile.isOpen()) {
+				console.consoleAdd(memStorage);
+				try {
+					Exporter self;
+					self.initFile(currentfile);
+					self.outCsv(1, memStorage);
+					self.~Exporter();
+				} catch (int e) {
+					if (e == CPPKONTAKTE_FILE_IO_ERROR) {
+						std::cout << "Error: file sync failed: file cannot be opened.";
+					}
+				}
+			} else {
+				std::cout << "No file has been opened yet" << std::endl;
+			}
+		} else if (console.lastCommand() == "edit") {
+			if(ifile.isOpen()) {
+				console.consoleEdit(memStorage);
+				try {
+					Exporter self;
+					self.initFile(currentfile);
+					self.outCsv(1, memStorage);
+					self.~Exporter();
+				} catch (int e) {
+					if (e == CPPKONTAKTE_FILE_IO_ERROR) {
+						std::cout << "Error: file sync failed: file cannot be opened.";
+					}
+				}
+			} else {
+				std::cout << "No file has been opened yet" << std::endl;
+			}
+		} else if (console.lastCommand() == "delete") {
+			if(ifile.isOpen()) {
+				console.consoleDelete(memStorage);
+				try {
+					Exporter self;
+					self.initFile(currentfile);
+					self.outCsv(1, memStorage);
+					self.~Exporter();
+				} catch (int e) {
+					if (e == CPPKONTAKTE_FILE_IO_ERROR) {
+						std::cout << "Error: file sync failed: file cannot be opened.";
+					}
+				}
+			} else {
+				std::cout << "No file has been opened yet" << std::endl;
+			}
+		} else if (console.lastCommand() == "search") {
+			if(ifile.isOpen()) {
+				console.consoleSearch(memStorage);
+			} else {
+				std::cout << "No file has been opened yet" << std::endl;
+			}
+		} else {
+			std::cout << "Unknown command." << std::endl;
 		}
 	}
     return 0;
