81f26da377b59d7ba96e8a6e2bce6bbe823b0a89
Samuel Ch <me@chsamuel.net>
2020-05-17 14:46:18 +0200

    removed and changed some validator functions

--- include/cppConsole.hpp
+++ include/cppConsole.hpp
@@ -5,12 +5,10 @@ class TerminalHandler {
 	std::string prompt;
 	bool loop;
 	std::string* askData(contactStore& cs); 
-	void validateEmail(std::string) { };
-	void validatePhone(std::string) { };
-	void validateNoNum(std::string) { };
-	void validateOnlyNum(std::string) { };
-	void display(cppContact&) { };
-	void displayFound(cppContact*) { };
+	bool validateEmail(const char*);
+	bool validatePhone(const char*);
+	bool validatePostal(const char*);
+	void display(cppContact&);
 public:
 	TerminalHandler(bool b = true) : loop(b) {}
 	void getCommand();
@@ -18,7 +16,7 @@ public:
 	void consoleEdit(contactStore& cs);
 	void consoleDelete(contactStore& cs);
 	void consoleSearch(contactStore& cs);
-	void displayAll(contactStore& cs) { };
+	void displayAll(contactStore& cs);
 	bool getLoop() { return loop; }
 	void setLoop(bool b) { loop = b; }
 	std::string lastCommand() { return prompt; }
